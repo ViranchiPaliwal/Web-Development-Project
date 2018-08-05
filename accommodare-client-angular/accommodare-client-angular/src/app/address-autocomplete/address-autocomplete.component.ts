e1c1d641277fb03c0231684c116842673c78d3a4
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-08-04 17:35:38 -0700

    address autocomplete componenet

---
+++ accommodare-client-angular/accommodare-client-angular/src/app/address-autocomplete/address-autocomplete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-autocomplete',
+  templateUrl: './address-autocomplete.component.html',
+  styleUrls: ['./address-autocomplete.component.css']
+})
+export class AddressAutocompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
